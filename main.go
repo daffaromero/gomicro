309f6958756ddd668f6a82c78ad841893aa43a5d
Daffa Muhammad Romero <daffaromero@mail.ugm.ac.id>
2024-07-13 18:38:08 +0700

    Very first service with Go standard libs

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World!")
+		d, _ := io.ReadAll(r.Body)
+
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Goodbye World!")
+	})
+
+	http.ListenAndServe("localhost:8086", nil)
+}
