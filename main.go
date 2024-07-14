1d3932340e6df0d30279a911f90ae1d766ca7b41
Daffa Muhammad Romero <daffaromero@mail.ugm.ac.id>
2024-07-14 20:31:52 +0700

    I think I GET it

--- main.go
+++ main.go
@@ -14,12 +14,10 @@ import (
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
-	hh := handlers.NewHello(l)
-	gh := handlers.NewGoodbye(l)
+	ph := handlers.NewProducts(l)
 
 	sm := http.NewServeMux()
-	sm.Handle("/", hh)
-	sm.Handle("/goodbye", gh)
+	sm.Handle("/", ph)
 
 	s := &http.Server{
 		Addr:         "localhost:8086",
@@ -43,6 +41,11 @@ func main() {
 	sig := <-sigChan
 	l.Println("Received terminate, graceful shutdown", sig)
 
-	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	tc, err := context.WithTimeout(context.Background(), 30*time.Second)
+
+	if err != nil {
+		l.Fatal(err)
+	}
+
 	s.Shutdown(tc)
 }
