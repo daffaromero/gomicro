7f049f5c27b581e8e11fc9d1fa92c1fdc5c32bec
Daffa Muhammad Romero <daffaromero@mail.ugm.ac.id>
2024-07-21 18:36:20 +0700

    gRPC starter

---
+++ currency/server/currency.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"context"
+
+	"github.com/daffaromero/gomicro/currency/protos/currency"
+	protos "github.com/daffaromero/gomicro/currency/protos/currency"
+	"github.com/hashicorp/go-hclog"
+)
+
+type Currency struct {
+	log hclog.Logger
+	currency.UnimplementedCurrencyServer
+}
+
+func NewCurrency(l hclog.Logger) *Currency {
+	return &Currency{
+		log: l,
+	}
+}
+
+func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos.RateResponse, error) {
+	c.log.Info("Handle req GetRate", "base", rr.GetBase(), "destination", rr.GetDestination())
+
+	return &protos.RateResponse{Rate: 0.5}, nil
+}
